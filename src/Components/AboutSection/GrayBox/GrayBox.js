f80f9efec5aadbfb6ece1bcda4a6be4425d666a7
edavetisyan <edmondavetisyanw@gmail.com>
2022-02-19 08:49:02 +0300

    fix: about animation with IntersectionObserver

--- src/Components/AboutSection/GrayBox/GrayBox.js
+++ src/Components/AboutSection/GrayBox/GrayBox.js
@@ -1,49 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./GrayBox.css";
 function GrayBox(props) {
-  const [translate, setTranslate] = useState(80);
-  const [opacity, setOpacity] = useState(0);
-  
-  let animateHeadings = () => {
-    setTimeout(() => {
-      setOpacity(1);
-      setTranslate(0);
-    }, 500);
-  };
-  
   useEffect(() => {
-    const onScroll = (e) => {
-      
-      if (e.target.documentElement.scrollTop) {
-        animateHeadings();
-      }
-      // else if(e.target.location){
-
-      // }
-    };
-
-    window.addEventListener("scroll", onScroll);
-    let about = document.querySelector('#about')
-    about.addEventListener('mouseover', animateHeadings)
-    return () => window.removeEventListener("scroll", onScroll);
+    function onEntry(entry) {
+      entry.forEach((change) => {
+        if (change.isIntersecting) {
+          if (change.target.localName === "h5") {
+            setTimeout(() => {
+              change.target.style.opacity = 1;
+              change.target.style.transform = "translateX(0px) translateY(0px)";
+            }, 750);
+          } else {
+            change.target.style.opacity = 1;
+            change.target.style.transform = "translateX(0px) translateY(0px)";
+          }
+        }
+      });
+    }
+    let options = { threshold: [0.5] };
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = document.querySelectorAll(".animatedTitle");
+    for (let elm of elements) {
+      observer.observe(elm);
+    }
   }, []);
 
   return (
     <div className="grayBox-cont">
       <div className="grayBox">
         <h2
+          className="h2 animatedTitle"
           style={{
-            transform: `translateX(0px) translateY(${translate}px)`,
+            transform: `translateX(0px) translateY(80px)`,
             transition: `opacity 750ms ease 0s, transform 500ms ease 0s`,
-            opacity: opacity,
+            opacity: 0,
           }}
         >
           {props.title}
         </h2>
         <h5
+          className="h5 animatedTitle"
           style={{
-            opacity: opacity,
-            transform: `translateX(${-translate}px) translateY(0px)`,
+            opacity: 0,
+            transform: `translateX(-50px) translateY(0px)`,
             transition: "opacity 750ms ease 0s, transform 750ms ease 0s",
           }}
         >
