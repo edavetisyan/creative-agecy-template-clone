fffee2e1bdfd6e225363633b04bc67161b2b8ac1
edavetisyan <edmondavetisyanw@gmail.com>
2022-02-26 20:14:31 +0300

    feat: portfolio section end

--- src/Components/PortfolioSection/PortfolioItem/PortfolioItem.js
+++ src/Components/PortfolioSection/PortfolioItem/PortfolioItem.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import './PortfolioItem.css'
+import React, { useState } from "react";
+import "./PortfolioItem.css";
 function PortfolioItem(props) {
-    
+  const [opacity, setOpacity] = useState(false);
   return (
-    <div className='PortfolioItem' onMouseEnter={()=>console.log(props)}>
-        <img src={props.img} alt={props.img}  />
+    <div
+      className="PortfolioItem"
+      onMouseEnter={() => setOpacity((prev) => !prev)}
+      onMouseLeave={() => setOpacity((prev) => !prev)}
+    >
+      <img src={props.img} alt={props.img} />
+      <div className="hover-inner" >
+        <div className="color" style={{ opacity: opacity ? 1 : 0}}>
+          <div className="inner-portfolio">
+            <p>
+              <span>
+                PORTFOLIO TITLE
+                <br />
+                Branding, Web Design
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default PortfolioItem
+export default PortfolioItem;
